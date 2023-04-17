262930af103c72258fc9d5c900b23c020de416c7
jiten vyas <jitenvyas619@gmail.com>
2023-04-17 17:28:14 +0530

    initial

--- actions.js
+++
@@ -1,6 +0,0 @@
-// actions.js
-export const UPDATE_WALLET_AMOUNT = "UPDATE_WALLET_AMOUNT";
-
-export const updateWalletAmount = (amountToAdd) => {
-  return { type: UPDATE_WALLET_AMOUNT, payload: amountToAdd };
-};
